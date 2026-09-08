0346727446b145a7bb955412f34febb34bdc4923
LucassFelipe <78313987+LucassFelipe@users.noreply.github.com>
2026-09-07 22:27:33 -0300

    fix: swipe com touch events puros (remove gesture-handler e reanimated do gesto)

--- src/components/SwipeableArea.tsx
+++ src/components/SwipeableArea.tsx
@@ -1,6 +1,6 @@
-import { Gesture, GestureDetector } from "react-native-gesture-handler"
 import { useRef } from "react"
-import { Animated, View } from "react-native"
+import { View } from "react-native"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 type Props = {
   children: React.ReactNode
@@ -8,25 +8,20 @@ type Props = {
 }
 
 export default function SwipeableArea({ children, onSwipe }: Props) {
-  const tx = useRef(new Animated.Value(0)).current
-
-  const g = Gesture.Pan()
-    .onUpdate((e) => {
-      tx.setValue(e.translationX * 0.4)
-    })
-    .onEnd((e) => {
-      if (e.translationX < -60) onSwipe(1)
-      else if (e.translationX > 60) onSwipe(-1)
-      Animated.timing(tx, { toValue: 0, duration: 150, useNativeDriver: true }).start()
-    })
+  const startX = useRef(0)
 
   return (
-    <GestureDetector gesture={g}>
-      <View className="flex-1">
-        <Animated.View style={{ transform: [{ translateX: tx }] }} className="flex-1">
-          {children}
-        </Animated.View>
-      </View>
-    </GestureDetector>
+    <View
+      className="flex-1"
+      onStartShouldSetResponder={() => true}
+      onResponderGrant={(e) => { startX.current = e.nativeEvent.pageX }}
+      onResponderRelease={(e) => {
+        const diff = e.nativeEvent.pageX - startX.current
+        if (diff < -60) onSwipe(1)
+        else if (diff > 60) onSwipe(-1)
+      }}
+    >
+      {children}
+    </View>
   )
 }
